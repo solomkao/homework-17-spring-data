71378706f09d1aefdff908cb923cba76aeae9bbb
Oksana <solomkao@gmail.com>
2021-01-24 15:34:00 +0200

    Create new branch, rename main class

--- src/main/java/com/solomka/SpringDataApp.java
+++ src/main/java/com/solomka/SpringDataApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Homework17SpringDataApplication {
+public class SpringDataApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(Homework17SpringDataApplication.class, args);
+        SpringApplication.run(SpringDataApp.class, args);
     }
 
 }
