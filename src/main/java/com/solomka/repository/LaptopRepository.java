6f3fcf5ab2b32371231d1dd1337a40c99a842d81
Oksana <solomkao@gmail.com>
2021-01-26 21:17:55 +0200

    add style css and RequestParam annotaion

--- src/main/java/com/solomka/repository/LaptopRepository.java
+++ src/main/java/com/solomka/repository/LaptopRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface LaptopRepository  extends JpaRepository<Laptop, Integer>
 {
     List<Laptop> findByOrderByBrandDesc();
-    //@Query("SELECT l FROM Laptop l WHERE l.memoryRAM > 16")
+
     List<Laptop> findLaptopsByMemoryRAMGreaterThanEqual(Integer value);
 
     List<Laptop> findLaptopsByUsedEquals(Boolean used);
